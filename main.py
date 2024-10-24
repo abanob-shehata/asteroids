8b53f9c2b8f6209a5c74feec2e32e82e97c7adce
abanob-shehata <abanob.sheh@gmail.com>
2024-10-24 12:08:15 +0900

    add asteroids and player shooting

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
-    # Pygame init and variable instanciation
+    # Pygame init 
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Objects instanciation and groups set-up
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    running = True
+    asteroid_field = AsteroidField()
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    dt = 0
+    running = True
     
     # Game loop
     while running:
@@ -21,13 +39,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Updating objects
+        for obj in updatable:
+            obj.update(dt)
+            
+        for asteroid in asteroids:
+            if asteroid.iscolliding(player):
+                print("Game Over!")
+                sys.exit()
             
         # Screen black fill
         screen.fill((0,0,0))
         
-        # Drawing player
-        player.update(dt)
-        player.draw(screen)
+        # Drawing objects
+        for obj in drawable:
+            obj.draw(screen)
         
         # Updating display
         pygame.display.flip()
